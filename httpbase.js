9755393ff294e93bb294cdf561af1c0a92ca8f8e
Joaquin Martinez <joaco1509@gmail.com>
2020-05-04 16:22:52 -0300

    commit

--- httpbase.js
+++ httpbase.js
@@ -1,6 +1,12 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
 // Connection URL
 const url = 'mongodb://localhost:27017';
 
@@ -9,6 +15,8 @@ const dbName = 'baseDatos1';
 
 // Create a new MongoClient
 const client = new MongoClient(url);
+
+var datos;
 
 // Use connect method to connect to the Server
 client.connect(function(err) {
@@ -24,8 +32,21 @@ client.connect(function(err) {
     assert.equal(err, null);
     console.log("Found the following records");
     console.log(docs)
+    datos= JSON.stringify(docs);
+
   });
-
   client.close();
-
 });
+
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.write(datos);
+  res.end('HOLA\n');
+});
+
+
+server.listen(port, hostname, () => {
+  console.log('El servidor se esta ejecutando en http://${hostname}:${port}/');
+})
